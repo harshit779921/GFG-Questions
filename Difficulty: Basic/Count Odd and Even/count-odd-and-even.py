6284eefb5b03a207186af6437d97daf46f70555e
Harshit Pandey <108739074+harshit779921@users.noreply.github.com>
2025-10-28 07:59:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Count Odd and Even/count-odd-and-even.py
+++ Difficulty: Basic/Count Odd and Even/count-odd-and-even.py
@@ -1,11 +1,11 @@
 class Solution:
 	def countOddEven(self, arr):
 		#Code here
-		even = 0
-		odd = 0
-		for i in arr:
-		    if i%2==0:
-		        even = even+1
-		    else :
-		        odd = odd+1
-		return (odd,even)
+		even_num = 0
+		odd_num = 0
+		for num in arr:
+		    if num%2 == 0:
+		        even_num += 1
+		    else:
+		        odd_num += 1
+		return (odd_num, even_num)
