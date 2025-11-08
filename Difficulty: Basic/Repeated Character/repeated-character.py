16f3dd86c4c8a36c639178fe826082a14890ed2c
Harshit Pandey <108739074+harshit779921@users.noreply.github.com>
2025-11-08 19:42:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Repeated Character/repeated-character.py
@@ -0,0 +1,18 @@
+#User function Template for python3
+
+class Solution:
+    def firstRep(self, s):
+        # code here
+        dic = {}
+        
+        for i in s:
+            if i in dic:
+                dic[i]+=1
+            else:
+                dic[i]=1
+        
+        for key, value in dic.items():
+            if value > 1:
+                return key
+                
+        return -1
