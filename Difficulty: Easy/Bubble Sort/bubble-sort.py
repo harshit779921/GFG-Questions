76cc34b6178f1057ce75bc415027e90df4446c8d
Harshit Pandey <108739074+harshit779921@users.noreply.github.com>
2025-11-15 12:11:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bubble Sort/bubble-sort.py
@@ -0,0 +1,10 @@
+class Solution:
+    def bubbleSort(self,arr):
+        # code here
+        n = len(arr)
+        
+        for i in range(n-2, -1, -1):
+            for j in range(0, i+1):
+                if arr[j]>arr[j+1]:
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+                    
