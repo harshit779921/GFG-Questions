a21979e25f9c133171b8b62bc789894b3352e731
Harshit Pandey <108739074+harshit779921@users.noreply.github.com>
2025-12-03 07:37:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find element at a given Index/find-element-at-a-given-index.py
@@ -0,0 +1,10 @@
+
+from typing import List
+
+
+class Solution:
+    def findElementAtIndex(self, key : int, arr : List[int]) -> int:
+        # code here
+        for i in arr:
+            return arr[key]
+        
